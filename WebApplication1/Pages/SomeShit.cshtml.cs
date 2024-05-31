dead010ecf6e345122db1fcae84a1fab3927743e
Self-proclaimed Idiot <surokveliky@gmail.com>
2024-05-31 17:52:50 +0300

    Страницы

---
+++ WebApplication1/Pages/SomeShit.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection.Metadata.Ecma335;
+using WebApplication1.Models;
+namespace WebApplication1.Pages
+{
+    public class SomeShitModel : PageModel
+    {
+        private readonly ApplicationContext _context;
+        [BindProperty]
+        public Chamber c { get; set; }
+        public List<Chamber> chambers { get; set; }
+        public SomeShitModel(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            chambers = _context.chamber.ToList();
+        }
+        public IActionResult OnPost()
+        {
+            _context.chamber.Add(c);
+            _context.SaveChanges();
+            return RedirectToPage();
+        }
+    }
+}
