9c6457067c93a97420d024be5b8937013e32b112
neprokuratura <gohaha142@gmail.com>
2024-06-02 01:28:26 +0300

    ViewModel для Пациента

---
+++ ShizUslugi/Controllers/PatientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShizUslugi.Models;
+using ShizUslugi.VIewModels;
+
+namespace ShizUslugi.Controllers
+{
+	public class PatientController : Controller
+	{
+		public readonly ApplicationContext _context;
+		public PatientController(ApplicationContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			AllPatientViewModel data = new AllPatientViewModel();
+			data.Chambers = _context.chamber.ToList();
+			return View(data);
+		}
+	}
+}
---
+++ ShizUslugi/ViewModels/AllPatientViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ShizUslugi.Models;
+namespace ShizUslugi.VIewModels
+{
+    public class AllPatientViewModel
+    {
+        public List<Patient> Patients { get; set; }
+        public List<Doctor> Doctorss { get; set; }
+        public List<Doctor_Patient_id> DocPaitid { get; set; }
+        public List<Diagnosis> Diagnoses { get; set; }
+        public List<Patient_Diagnosis> PaitDiagid { get; set; }
+        public List<Chamber> Chambers { get; set; }
+    }
+}
