ce4eb69d85c4f897194c945bd0b05f407b4fde06
Self-proclaimed Idiot <surokveliky@gmail.com>
2024-06-03 00:32:25 +0300

    Сделал защиту от шизов

--- ShizUslugi/Controllers/DoctorController.cs
+++ ShizUslugi/Controllers/DoctorController.cs
@@ -13,38 +13,54 @@ namespace ShizUslugi.Controllers
 		}
 		public IActionResult Index()
 		{
-			AllDoctorViewModel model = new AllDoctorViewModel();
-			model.doctor = StaticStuff.doctor;
-			return View(model);
+			if (StaticStuff.status)
+			{
+				AllDoctorViewModel model = new AllDoctorViewModel();
+				model.doctor = StaticStuff.doctor;
+				return View(model);
+			}
+			else return RedirectToAction("PatientWarning");
 		}
 		[HttpGet]
 		public IActionResult MyPatients()
 		{
-			AllDoctorViewModel model = new AllDoctorViewModel();
-			List<Patient> p	= new List<Patient>();
-			List<Doctor_Patient_id> a = _context.doctor_and_patient.Where(a => a.doctorid == StaticStuff.doctor.id).ToList();
-			foreach (var v in a)
+			if (StaticStuff.status)
 			{
-				p.Add(_context.patient.Where<Patient>(b => b.id == v.patientid).ToList()[0]);
+				AllDoctorViewModel model = new AllDoctorViewModel();
+				List<Patient> p = new List<Patient>();
+				List<Doctor_Patient_id> a = _context.doctor_and_patient.Where(a => a.doctorid == StaticStuff.doctor.id).ToList();
+				foreach (var v in a)
+				{
+					p.Add(_context.patient.Where<Patient>(b => b.id == v.patientid).ToList()[0]);
+				}
+				model.chambers = _context.chamber.ToList();
+				model.patients = p;
+				return View(model);
 			}
-			model.chambers = _context.chamber.ToList();
-			model.patients = p;
-			return View(model);
+			else return RedirectToAction("PatientWarning");
 		}
 
 		[HttpGet]
 		public IActionResult MySchedule()
 		{
-			AllDoctorViewModel model = new AllDoctorViewModel();
-			model.schedules = _context.schedule.Where(b => b.doctorid == StaticStuff.doctor.id).ToList();
-			List<Patient> p = new List<Patient>();
-			
-			foreach (var v in model.schedules)
+			if (StaticStuff.status)
 			{
-				p.Add(_context.patient.Where<Patient>(c => c.id == v.patientid).ToList()[0]);
+				AllDoctorViewModel model = new AllDoctorViewModel();
+				model.schedules = _context.schedule.Where(b => b.doctorid == StaticStuff.doctor.id).ToList();
+				List<Patient> p = new List<Patient>();
+
+				foreach (var v in model.schedules)
+				{
+					p.Add(_context.patient.Where<Patient>(c => c.id == v.patientid).ToList()[0]);
+				}
+				model.patients = p;
+				return View(model);
 			}
-			model.patients = p;
-			return View(model);
+			else return RedirectToAction("PatientWarning");
+		}
+		public IActionResult PatientWarning()
+		{
+			return View();
 		}
 	}
 }
